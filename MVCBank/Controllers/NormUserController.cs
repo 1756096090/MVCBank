10b03b69b31924b895d104bfc150e5ba639a5a42
SebastianG03 <guamandavid11235@gmail.com>
2023-10-28 11:20:01 -0500

    Ultimos cambios
    reword 05545f3 Cambios realizados, styles temporale

--- MVCBank/Controllers/NormUserController.cs
+++ MVCBank/Controllers/NormUserController.cs
@@ -26,18 +26,26 @@ namespace MVCBank.Controllers
         public async Task<IActionResult> Index(string Email, string Password)
 
         {
-            
+
             var response = await servicioAPI.Autenticar(Email, Password);
-                Console.WriteLine("hola"+response?.IdUser);
+            Console.WriteLine("hola " + response?.IdUser);
             if (response?.IdUser != 0)
             {
-                var response2 = await servicio_APIBAccount.GetIdUser((int)(response?.IdUser));
-                Console.WriteLine("Resp"+response2?.IdUser);
-                return View("Details",response2);
+                if (response.Role.Equals("client"))
+                {
+                    var response2 = await servicio_APIBAccount.GetIdUser((int)(response?.IdUser));
+                    Console.WriteLine("Resp" + response2?.IdUser);
+                    return View("Details", response2);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Admin", response);
+                }
+
 
 
             }
-            return View(); 
+            return View();
         }
 
         // GET: NormUserController/Details/5
@@ -53,61 +61,101 @@ namespace MVCBank.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Details(int IdAccount)
+        {
+            var account = await servicio_APIBAccount.Obtener(IdAccount);
+            if (account != null)
+            {
+                return View("Edit", account);
+            }
+            Console.WriteLine("Fallo");
+            return RedirectToAction("Index");
+        }
+
         // GET: NormUserController/Create
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Create(User user)
         {
             user.Role = "string";
             user.IdUser = 0;
-            
-            user.Phone = "stringstri";
-            user.DNI = "string";
-            user.Name = "string";
+            user.Phone = "strddsfasd";
+            user.DNI = "strdddd";
+            user.Name = "strsssds";
+
+            user.Role = user.Role.Equals("Cliente") ? "client" : "adm";
+
             var response = await servicioAPI.Crear(user);
 
-            Console.WriteLine(user.Password+"hola");
-            if (response!=0)
-            {   
+            Console.WriteLine(user.Password + "hola");
+            if (response != 0)
+            {
                 BankAccount account = new BankAccount();
-                account.AccountNumber = 1;
+                var lista = await servicio_APIBAccount.Lista();
+                account.AccountNumber = lista.Count + 1;
                 account.AccountAmount = 0;
                 account.IdUser = response;
                 var response2 = await servicio_APIBAccount.Guardar(account);
 
-            return View("Index");
+                return View("Index");
 
-            } 
+            }
             return View();
 
         }
 
-       
+
 
 
 
         // GET: NormUserController/Edit/5
-        public ActionResult Edit(int id)
+        public async Task<ActionResult> Edit(int IdAccount)
         {
+
+            var response = await servicio_APIBAccount.Obtener(IdAccount);
+            Console.WriteLine("hola" + response?.IdUser);
+            if (response?.IdUser != 0)
+            {
+                Transferencia transfer = new Transferencia() { Amount = 0, IdAccountSender = response.IdAccount, IdAccountReceiver = 0 };
+
+                return View("Edit", transfer);
+
+
+            }
             return View();
+
         }
 
         // POST: NormUserController/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(Transferencia transfer)
         {
-            try
+            if (transfer != null)
             {
-                return RedirectToAction(nameof(Index));
+                transfer.DateIssue = DateTime.Now;
+                BankAccount accountSender = await servicio_APIBAccount.Obtener(transfer.IdAccountSender);
+                BankAccount accountReceiver = await servicio_APIBAccount.Obtener(transfer.IdAccountReceiver);
+
+                if (accountReceiver != null)
+                {
+                    accountReceiver.AccountAmount = transfer.Amount;
+                    Console.WriteLine("Transferencia emitida: ");
+                }
+
+                Console.WriteLine("Transferencia realizandose..." + accountReceiver.AccountAmount);
+                //await servicio_APIBAccount.Editar(accountSender);
+                bool success = await servicio_APIBAccount.Editar(accountReceiver);
+                Console.WriteLine("Transferencia realizada..." + success);
+
+                return View("Details", accountSender);
             }
-            catch
-            {
-                return View();
-            }
+            Console.WriteLine("Operacion fallida");
+            return BadRequest("No se pudo realizar la accion");
         }
 
         // GET: NormUserController/Delete/5
